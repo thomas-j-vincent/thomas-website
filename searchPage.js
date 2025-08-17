ec72999edf7e3742d91322f74fea23f6414a1422
thomas-j-vincent <thomasjvincent@outlook.com>
2025-08-17 22:49:40 +0100

    fixed an error so the rest of the code will run :|

--- searchPage.js
+++ searchPage.js
@@ -81,7 +81,7 @@ function displayResults(item) {
       if (results.length > 0) {
         results.forEach(product => {
           const div = document.createElement("div");
-          div.textContent = `${addProduct(item)}`;
+          div.textContent = `${item.name}`;
           resultsContainer.appendChild(div);
         });
       } else {
