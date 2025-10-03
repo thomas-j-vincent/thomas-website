b0ec2c81bd090a891dcb29ce848a10d16209ea85
thomas-j-vincent <thomasjvincent@outlook.com>
2025-10-03 12:27:19 +0100

    checkout displays products from basket

--- checkoutPage.js
+++ checkoutPage.js
@@ -1,8 +1,14 @@
 import { products } from "./products.js";
-import { get, set, updateBasketMessage, addProduct, formatPrice, enableTouchHover, loadFromStorage} from "./functions.js";
+import { get, set, updateBasketMessage, addProduct, formatPrice, enableTouchHover, loadFromStorage, universalDisplay} from "./functions.js";
 
 updateBasketMessage();
 enableTouchHover();
 loadFromStorage();
 
-console.log(get("basketProducts"));
+  const basketProducts = get("basketProducts") || [];
+ basketProducts.forEach(({ item }) => { //SAME LINE takes basket products and converts to item END
+    console.log(item.name);
+    let resultTable = document.getElementById("results")
+    let table = universalDisplay(item);
+    resultTable.appendChild(table);
+ });
